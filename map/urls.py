049334c53ac6b83b8a2906b5cd153b54b93db00c
Tyler Jackson <jacksty@gmx.com>
2018-02-14 17:34:03 -0500

    added edit view and index view

--- map/urls.py
+++ map/urls.py
@@ -5,6 +5,7 @@ from . import views
 urlpatterns = [
     path('', views.index),
     path('<int:map>/', views.index),
+    path('<int:map>/edit', views.edit),
     path('submit/node', views.submit_node),
     path('submit/edge', views.submit_edge),
     path('<int:map>/locations', views.update_locations),
--- map/views.py
+++ map/views.py
@@ -10,6 +10,23 @@ from .serializers import NodeSerializer, EdgeSerializer
 # Create your views here.
 
 def index(request, map=2):
+    template = loader.get_template('map/edit.html')
+    nodes = Node.objects.filter(map=map)
+    edges = Edge.objects.filter(map=map)
+    nodeSerializer = NodeSerializer(nodes, many=True)
+    edgeSerializer = EdgeSerializer(edges, many=True)
+
+    context = {
+        'nodes': json.dumps(nodeSerializer.data),
+        'edges': json.dumps(edgeSerializer.data),
+        'width': 900,
+        'height': 600,
+        'map': map,
+    }
+ 
+    return HttpResponse(template.render(context, request))
+
+def edit(request, map=2):
     template = loader.get_template('map/index.html')
     nodes = Node.objects.filter(map=map)
     edges = Edge.objects.filter(map=map)

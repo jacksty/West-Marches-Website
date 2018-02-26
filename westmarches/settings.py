6647d6d85ab681ecca6632255babbe8d9428ea1b
Tyler Jackson <jacksty@gmx.com>
2018-02-26 01:21:47 -0500

    postgres support test

--- westmarches/settings.py
+++ westmarches/settings.py
@@ -81,12 +81,12 @@ WSGI_APPLICATION = 'westmarches.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
+import dj_database_url
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+    'default': dj_database_url.config(conn_max_age=600)
 }
+
 
 
 # Password validation
